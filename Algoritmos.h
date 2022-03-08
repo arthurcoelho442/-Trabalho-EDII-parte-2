aafb64df3a84d2b9b676d2eec1eaa1b4c84441b5
Arthur Coelho Estevão <56831082+arthurcoelho442@users.noreply.github.com>
2022-03-07 23:02:38 -0300

    Comentarios

--- Algoritmos.h
+++ Algoritmos.h
@@ -3,14 +3,28 @@
 
 #include "suffix.h"
 
+// Algoritmo de ordenação bubblesort
 void bubblesort(Suffix** a, int n);
+
+// Algoritmo de ordenação selectionsort
 void selectionsort(Suffix** a,int n);
+
+// Algoritmo de ordenação insertionsort
 void insertionsort(Suffix** a, int n);
+
+// Algoritmo de ordenação shellsort
 void shellsort(Suffix** a,int n);
+
+// Algoritmo de ordenação quicksort
 void quicksort(Suffix** a, int l, int r);
+
+// Algoritmo de ordenação heapsort
 void heapsort(Suffix** a, int l, int r);
 
+//Imprime as estatisticas do algoritmo
 void printCounters(char *s, double time);
+
+//Reseta o valos dos contadores
 void resetCounters();
 
 #endif
